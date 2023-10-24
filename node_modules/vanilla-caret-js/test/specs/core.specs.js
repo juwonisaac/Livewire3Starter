5a6b21ec0a4fb597c53667aea484b3885c05ce3d
juwonisaac <127780188+juwonisaac@users.noreply.github.com>
2023-10-24 07:29:45 +0100

    Delete node_modules directory

--- node_modules/vanilla-caret-js/test/specs/core.specs.js
+++
@@ -1,8 +0,0 @@
-/* global Greeter */
-describe('Core', function() {
-  it('VanillaCaret exsists', () => expect(VanillaCaret).is.not.undefined )
-
-  it('it can create caret js objects', () => {
-    //TODO: Write tests
-  })
-})
